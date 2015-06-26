d74ccc1f4934a78a3b41021d42d60865c22becd8
Chad Meyer <chadmeyer@me.com>
2015-06-26 16:17:09 -0500

    Still had some issues with serialization on ccb address.

---
+++ src/ChurchCommunityBuilder.Api/People/Entity/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using ChurchCommunityBuilder.Api.Entity;
+
+namespace ChurchCommunityBuilder.Api.People.Entity {
+    [XmlRoot("country")]
+    public class Country {
+        [XmlAttribute("code")]
+        public string Code { get; set; }
+
+        [XmlText]
+        public string Value { get; set; }
+    }
+}
