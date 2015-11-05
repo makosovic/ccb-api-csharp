c3407a445ad8b4eeeffbabf96125ad504560addf
Chad Meyer <chadmeyer@me.com>
2015-11-05 01:38:40 -0600

    Taking out the dependency of Restify.

--- ChurchCommunityBuilder.Api.Tests/Integration/Events/CalendarTests.cs
+++ ChurchCommunityBuilder.Api.Tests/Integration/Events/CalendarTests.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Shouldly;
 using NUnit.Framework;
-using ChurchCommunityBuilder.Api;
-using ChurchCommunityBuilder.Api.People.Entity;
-using ChurchCommunityBuilder.Api.People.QueryObject;
+using Shouldly;
 
 
 namespace ChurchCommunityBuilder.Api.Tests.Integration.Events {
