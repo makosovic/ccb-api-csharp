6018cc222afe7685e1e26af3f953c5bded81f78b
Brandon Krauska <bkrauska@gmail.com>
2017-11-03 10:08:24 -0500

    Added some people search and get tests for ccb

---
+++ tests/ChurchCommunityBuilder.Api.Tests/Integration/People/GetTests.cs
@@ -0,0 +1,21 @@
+﻿using Shouldly;
+using NUnit.Framework;
+
+namespace ChurchCommunityBuilder.Api.Tests.Integration.People {
+    [TestFixture]
+    public class GetTests : BaseTest {
+        [Test]
+        public void integration_people_get_include_inactive_false() {
+            var id = 776;
+            var individual = base.ApiClient.People.Individuals.Get(id, false);
+            individual.ShouldNotBeNull();
+        }
+
+        [Test]
+        public void integration_people_get_include_inactive_true() {
+            var id = 776;
+            var individual = base.ApiClient.People.Individuals.Get(id, true);
+            individual.ShouldNotBeNull();
+        }
+    }
+}
---
+++ tests/ChurchCommunityBuilder.Api.Tests/Integration/People/SearchTests.cs
@@ -0,0 +1,22 @@
+﻿using Shouldly;
+using NUnit.Framework;
+
+namespace ChurchCommunityBuilder.Api.Tests.Integration.People {
+    [TestFixture]
+    public class SearchTests : BaseTest {
+        [Test]
+        public void integration_people_search_first_and_last_include_inactive_false() {
+            string firstName = "Dick";
+            string lastName = "Wersch";
+
+            var qo = new Api.People.QueryObject.IndividualQO {
+                FirstName = firstName,
+                LastName = lastName,
+                IncludeInactive = false
+            };
+
+            var results = base.ApiClient.People.Individuals.Search(qo);
+            results.Individuals.Count.ShouldBe(1);
+        }
+    }
+}
