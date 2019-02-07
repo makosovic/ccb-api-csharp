b870cc3d2c3ce7d60f57caf1268cad21d8f6dd49
Marko Kosović <marko.kosovic@toptal.com>
2019-02-07 08:55:25 +0100

    Fixed rate limit reset datetime value.

--- src/ChurchCommunityBuilder.Api/Extensions/RestSharpExtensions.cs
+++ src/ChurchCommunityBuilder.Api/Extensions/RestSharpExtensions.cs
@@ -1,56 +1,71 @@
 ﻿using ChurchCommunityBuilder.Api.Entity;
 using RestSharp;
+using System;
 using System.Linq;
 
-namespace ChurchCommunityBuilder.Api.Extensions {
-    public static class RestSharpExtensions {
-        public static IChurchCommunityBuilderResponse ToChurchCommunityBuilderResponse(this IRestResponse restResponse) {
+namespace ChurchCommunityBuilder.Api.Extensions
+{
+    public static class RestSharpExtensions
+    {
+        public static IChurchCommunityBuilderResponse ToChurchCommunityBuilderResponse(this IRestResponse restResponse)
+        {
             var response = new ChurchCommunityBuilderResponse();
 
             response.StatusCode = restResponse.StatusCode;
             response.JsonResponse = restResponse.Content;
 
-            if (restResponse.StatusCode != System.Net.HttpStatusCode.OK) {
+            if (restResponse.StatusCode != System.Net.HttpStatusCode.OK)
+            {
                 response.ErrorMessage = restResponse.ErrorMessage;
             }
 
             return response;
         }
 
-        public static IChurchCommunityBuilderResponse<S> ToChurchCommunityBuilderResponse<S>(this IRestResponse<S> restResponse) where S : new() {
+        public static IChurchCommunityBuilderResponse<S> ToChurchCommunityBuilderResponse<S>(this IRestResponse<S> restResponse) where S : new()
+        {
             var response = new ChurchCommunityBuilderResponse<S>();
 
             response.StatusCode = restResponse.StatusCode;
             response.JsonResponse = restResponse.Content;
 
-            if (restResponse.Headers != null && restResponse.Headers.Count > 0) {
-               if (restResponse.Headers.Any(x => x.Name == "X-RateLimit-Limit")) {
+            if (restResponse.Headers != null && restResponse.Headers.Count > 0)
+            {
+                if (restResponse.Headers.Any(x => x.Name == "X-RateLimit-Limit"))
+                {
                     response.RateLimit = restResponse.Headers.First(x => x.Name == "X-RateLimit-Limit").Value.ToInt();
                 }
 
-                if (restResponse.Headers.Any(x => x.Name == "X-RateLimit-Remaining")) {
+                if (restResponse.Headers.Any(x => x.Name == "X-RateLimit-Remaining"))
+                {
                     response.RateLimitRemaining = restResponse.Headers.First(x => x.Name == "X-RateLimit-Remaining").Value.ToInt();
                 }
 
-                if (restResponse.Headers.Any(x => x.Name == "X-RateLimit-Reset")) {
-                    response.RateLimitReset = new System.DateTime(restResponse.Headers.First(x => x.Name == "X-RateLimit-Reset").Value.ToLong());
+                if (restResponse.Headers.Any(x => x.Name == "X-RateLimit-Reset"))
+                {
+                    var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+                    response.RateLimitReset = epoch.AddSeconds(restResponse.Headers.First(x => x.Name == "X-RateLimit-Reset").Value.ToLong());
                 }
 
-                if (restResponse.Headers.Any(x => x.Name == "Retry-After")) {
+                if (restResponse.Headers.Any(x => x.Name == "Retry-After"))
+                {
                     response.RetryAfter = restResponse.Headers.First(x => x.Name == "Retry-After").Value.ToInt();
                 }
             }
 
-            if ((int)restResponse.StatusCode >= 300) {
+            if ((int)restResponse.StatusCode >= 300)
+            {
                 response.ErrorMessage = restResponse.ErrorMessage;
             }
-            else {
+            else
+            {
                 response.Data = restResponse.Data;
             }
             return response;
         }
 
-        public static IChurchCommunityBuilderResponse<S> ToChurchCommunityBuilderResponse<S>(this IRestResponse<S> restResponse, string requestInput) where S : new() {
+        public static IChurchCommunityBuilderResponse<S> ToChurchCommunityBuilderResponse<S>(this IRestResponse<S> restResponse, string requestInput) where S : new()
+        {
             var response = restResponse.ToChurchCommunityBuilderResponse();
             response.RequestValue = requestInput;
             return response;
