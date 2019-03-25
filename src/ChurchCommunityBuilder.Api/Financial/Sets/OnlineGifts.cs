43f34a610292115524c152f35c33481341b8040a
Marko Kosović <marko.kosovic@locco.hr>
2019-03-25 14:39:01 +0100

    Fixed online gifts being encoded and released new version.

--- src/ChurchCommunityBuilder.Api/Financial/Sets/OnlineGifts.cs
+++ src/ChurchCommunityBuilder.Api/Financial/Sets/OnlineGifts.cs
@@ -6,12 +6,15 @@ using System.Threading.Tasks;
 using ChurchCommunityBuilder.Api.Financial.Entity;
 using ChurchCommunityBuilder.Api.Entity;
 
-namespace ChurchCommunityBuilder.Api.Financial.Sets {
-    public class OnlineGifts : BaseApiSet<OnlineGift> {
+namespace ChurchCommunityBuilder.Api.Financial.Sets
+{
+    public class OnlineGifts : BaseApiSet<OnlineGift>
+    {
         public OnlineGifts(string baseUrl, string username, string password) : base(baseUrl, username, password) { }
 
-        public IChurchCommunityBuilderResponse<OnlineGift> Create(OnlineGift entity) {
-            var result = this.Execute<OnlineGift>("online_giving_insert_gift", entity.GetFormDictionary());
+        public IChurchCommunityBuilderResponse<OnlineGift> Create(OnlineGift entity)
+        {
+            var result = this.Execute("online_giving_insert_gift", entity.GetFormValues(), entity.GetFormDictionary());
             return result;
         }
     }
