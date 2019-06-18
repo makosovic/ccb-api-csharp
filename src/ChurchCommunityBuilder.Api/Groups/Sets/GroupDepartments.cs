710ceb8b72129924cdad27271b0caec19906fdca
Marko Kosović <marko.kosovic@locco.hr>
2019-06-18 09:16:51 +0200

    Added group departments and types lookup.

---
+++ src/ChurchCommunityBuilder.Api/Groups/Sets/GroupDepartments.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChurchCommunityBuilder.Api.Groups.QueryObject;
+using ChurchCommunityBuilder.Api.Groups.Entity;
+using ChurchCommunityBuilder.Api.Entity;
+
+namespace ChurchCommunityBuilder.Api.Groups.Sets {
+    public class GroupDepartments : BaseApiSet<LookupTypeCollection>
+    {
+        public GroupDepartments(string baseUrl, string username, string password) : base(baseUrl, username, password) { }
+
+        public IChurchCommunityBuilderResponse<LookupTypeCollection> List()
+        {
+            return this.Execute("group_grouping_list");
+        }
+    }
+}
---
+++ src/ChurchCommunityBuilder.Api/Groups/Sets/GroupTypes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChurchCommunityBuilder.Api.Groups.QueryObject;
+using ChurchCommunityBuilder.Api.Groups.Entity;
+using ChurchCommunityBuilder.Api.Entity;
+
+namespace ChurchCommunityBuilder.Api.Groups.Sets {
+    public class GroupTypes : BaseApiSet<LookupTypeCollection>
+    {
+        public GroupTypes(string baseUrl, string username, string password) : base(baseUrl, username, password) { }
+
+        public IChurchCommunityBuilderResponse<LookupTypeCollection> List()
+        {
+            return this.Execute("group_type_list");
+        }
+    }
+}
